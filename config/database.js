0deae4a3e8c490e11486c21197856d1e8f4c3bdc
Fernando <84139372+Fernando1403@users.noreply.github.com>
2022-03-24 09:33:59 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const conexao = async() => {
 
-var atlas = await mongoose.connect('COlOQUE SEU LINK DO MONGO DB')
+var atlas = await mongoose.connect('mongodb+srv://Fernando1403:Cluster111@cluster1.uman3.mongodb.net/ac1tri')
 }
 
 module.exports = conexao
